1a7f2714faeb07af4c80c3f574dc0138a5a379cb
Krystian Jagiełło <113018723+jagiellokrystian@users.noreply.github.com>
2024-03-18 13:16:18 +0100

    Add files via upload

---
+++ KrystianJag/script.js
@@ -0,0 +1,130 @@
+1.
+/*var tab = [5,12,17,23,123,45,54];
+console.log(tab); */
+
+
+2. 
+/*var tab = [5,12,17,23,123,45,54];
+console.log(tab); */
+
+
+3.
+/*tab = [5,12,17,23,123,45,54];
+let output = ' ';
+for(let i = 0; i < tab.length; i++) {
+    output += tab[i];
+    if(i !== tab.lenght) {
+        output += ' ,';
+    }
+}
+
+document.getElementById("tablicaDane").innerHTML = output;*/
+
+
+4. 
+/*const tab = [5,12,17,23,123,45,54];
+for(const num of tab) {
+    document.getElementById("tablica").innerText += num + ' ';
+}*/
+
+
+5. 
+/*const tab = [5,12,17,23,123,45,54];
+console.log(tab);
+tab.push(2, 201);
+console.log(tab);*/
+
+
+6.
+/*const tab = [5,12,17,23,123,45,54];
+tab.push(33,44);
+console.log("dlugosc: ", tab.length);
+console.log("zawartosc: ", tab);*/
+
+
+7.
+/*const tab = [5,12,17,23,123,45,54];
+tab.pop();
+console.log("Dlugosc: ", tab.length);
+console.log("Zawartosc: ", tab);*/
+
+
+8.
+/*const tab = [5,12,17,23,123,45,54];
+console.log(tab);
+tab.unshift(7,77);
+console.log("Dlugosc: ", tab.length);
+console.log("Zawartosc: ", tab);*/
+
+
+9.
+/*const tab = [5,12,17,23,123,45,54];
+console.log(tab);
+tab.shift();
+console.log("Dlugosc: ", tab.length);
+console.log("Zawartosc: ", tab);*/
+
+
+10.
+/*const tab = [5,12,17,23,123,45,54];
+console.log(tab);
+tab.reverse();
+console.log(tab);*/
+
+
+11.
+/*const tab = [5, 94, 12, 2, 30];
+console.log(tab.sort());*/
+
+
+12.
+/*const tab = [5, 94, 12, 2, 30];
+tab.forEach((Element, index, Array) => {
+    console.log("Element: ", Element);
+    console.log("index: ", index);
+    console.log("Array: ", Array);
+});*/
+
+
+13.
+/*const tab = [5, 94, 12, 2, 30];
+tab.forEach((Element, index, Array) => {
+    console.log("Element: ", Element);
+    console.log("index: ", index);
+    console.log("Array: ", Array);
+});*/
+
+
+14.
+/*const tab = [5, 94, 12, 2, 30];
+tab.forEach((Element, index, Array) => {
+    console.log("Element: ", Element);
+    console.log("index: ", index);
+    console.log("Array: ", Array);
+});*/
+
+
+15.
+/*const tab = [5, 94, 12, 2, 30];
+tab.forEach((Element, index, Array) => {
+    console.log("Element: ", Element);
+    console.log("index: ", index);
+    console.log("Array: ", Array);
+});*/
+
+
+16.
+/*const tab = ["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"];
+tab.reverse();
+console.log(tab);*/
+
+
+17.
+/*const tab = ["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"];
+console.log(tab.join(" ,"));
+console.log(tab.join( "\n"));*/
+
+
+18.
+/*const tab = ["aaa bbb","aaa bbb","aaa bbb","aaa bbb","aaa bbb","aaa bbb"];
+console.log(tab);*/
